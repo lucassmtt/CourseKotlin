e307abec5759d9047a3283a76440bfad0ed2ded6
Lucas Motta <110114441+lucassmtt@users.noreply.github.com>
2023-05-23 10:33:34 -0300

    Update WhileLoops.kt

--- src/Exercise_03_CoditionsAndLoops/WhileLoops.kt
+++ src/Exercise_03_CoditionsAndLoops/WhileLoops.kt
@@ -2,13 +2,22 @@ package Exercise_03_CoditionsAndLoops
 
 fun main(){
 
-    var x: Int = 10;
+//    var x: Int = 10;
+//
+//    while (x > 0){
+//
+//        println("The x value is $x")
+//        x --
+//    }
 
-    while (x > 0){
+    var option: Int = 0
 
-        println("The x value is $x")
-        x --
+    do {
+        print("Please, send one value: ")
+        val line = readLine() ?: "0"
+        option = line.toIntOrNull() ?: 0
+        println("You sent the value $option!! ")
     }
-
+    while(option != -1)
 
 }
